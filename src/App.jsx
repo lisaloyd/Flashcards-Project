89530709672f65a32fdc2b5353e03f60c77361a8
unknown <greateducation123@yahoo.com>
2023-02-08 16:11:20 -0600

    Created Axios GET request to backend API to see each available flashcard within a collection + created Axios GET request to backend API to see each available collection with its title

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [flashcards, setFlashcards] = useState([]);
+  const [collections, setCollections] = useState([]);
+
+
+  useEffect (() => {
+    getAllCollections();
+    getCardsForCollections();
+  }, [])
+
+  async function getAllCollections(){
+      let response = await axios.get('http://127.0.0.1:8000/api/collections/');
+      setCollections(response.data);
+  } 
+
+  async function getCardsForCollections(){
+    let response = await axios.get('http://127.0.0.1:8000/api/collections/2/cards/');
+    setFlashcards(response.data);
+  }
+  
   return (
     <div>
-      <h3>Hello world</h3>
+      
     </div>
   );
 }
